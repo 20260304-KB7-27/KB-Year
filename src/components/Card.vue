496fe57874215aa2d747e4a5e81368ab35964ad7
kchun0513 <kchun0513@hanyang.ac.kr>
2026-04-08 00:19:22 +0900

    CAV-41 [style] Card 내부 컨텐츠 중앙 정렬 및 캘린더 스타일 수정

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div
-    class="w-80 p-8 rounded-[40px] bg-[#f4f2ee] shadow-[15px_15px_30px_#d9d5ce,-15px_-15px_30px_#ffffff]"
+    class="w-80 p-8 rounded-[40px] bg-[#f4f2ee] shadow-[15px_15px_30px_#d9d5ce,-15px_-15px_30px_#ffffff] content-center"
   >
     <slot />
   </div>
