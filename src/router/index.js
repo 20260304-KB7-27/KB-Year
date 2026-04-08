bd4c281fc19836f4560a33d59206284bfb70219e
kchun0513 <62457974+kchun0513@users.noreply.github.com>
2026-04-08 09:29:19 +0900

    feat: Router 설정 변경 및 vercel.json 추가 [CAV-59]

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: LandingPage },
-    { path: '/dashboard', component: MainPage },
+    { path: '/', component: MainPage },
+    { path: '/login', component: LandingPage },
   ],
 });
 
