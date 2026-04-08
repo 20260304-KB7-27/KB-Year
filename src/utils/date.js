a17faca9a5d32612b5e8d302ba64e7a05d7fa941
1juyoung <myjuyoung2@gmail.com>
2026-04-08 16:45:13 +0900

    feat: formatQueryDate 함수 추가 [CAV-83]

--- src/utils/date.js
+++ src/utils/date.js
@@ -5,6 +5,12 @@ export const isSameDate = (d1, d2) => {
     d1.getDate() === d2.getDate()
   );
 };
+export const formatQueryDate = (date) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+};
 
 export const formatTime = (dateString) => {
   const date = new Date(dateString);
