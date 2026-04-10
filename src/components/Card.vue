6501f4d4e0f972368f8e3ce4e77c69c424969d4d
Nahyun <65719936+nhkim06@users.noreply.github.com>
2026-04-10 11:18:15 +0900

    style: 모바일 화면 패딩 넣기 [CAV-98] (#111)
    * style: 공통 스타일 통합 [CAV-98]
    
    * style: 거래 목록 공통 스타일 적용 [CAV-98]
    
    * style: change icons [CAV-98]
    
    * style: 모바일 화면 패딩 넣기 [CAV-98]

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div
-    class="w-80 p-8 rounded-[40px] bg-[#f4f2ee] shadow-[15px_15px_30px_#d9d5ce,-15px_-15px_30px_#ffffff] content-center card-effect"
+    class="w-80 p-8 max-w-[400px] rounded-[40px] bg-[#f4f2ee] shadow-[15px_15px_30px_#d9d5ce,-15px_-15px_30px_#ffffff] content-center card-effect"
   >
     <slot />
   </div>
