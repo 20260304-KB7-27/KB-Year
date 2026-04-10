16073d701568cb44d8af2fc50b61ab6fd3801820
1juyoung <myjuyoung2@gmail.com>
2026-04-10 15:55:48 +0900

    feat: pwa 설치 안내 toast 설정 [CAV-113]

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,7 +1,7 @@
 <script setup>
 import draggable from 'vuedraggable';
 
-import { ref, computed, watch, onMounted } from 'vue';
+import { h, ref, computed, onMounted, onBeforeUnmount } from 'vue';
 import { today, getLocalTimeZone } from '@internationalized/date';
 
 import { Calendar } from '@/components/ui/calendar';
@@ -16,11 +16,99 @@ import { useBarChartStore } from '@/stores/barChart';
 import TimeLine from '@/components/tradeList/TimeLine.vue';
 
 import { useDurationStore } from '@/stores/duration';
+
+import { toast } from 'vue-sonner';
 // 상태 관리
 const userStore = useUserStore(); // 유저 정보 관리
 const user = computed(() => userStore.user);
 const durationStore = useDurationStore(); // 수입/지출 내역 기간 관리
 const barChartStore = useBarChartStore(); // barChart 데이터 관리
+
+// pwa 설치
+const deferredPrompt = ref(null);
+const canInstall = ref(false);
+
+const handleBeforeInstallPrompt = (event) => {
+  event.preventDefault();
+  deferredPrompt.value = event;
+  canInstall.value = true;
+};
+
+const installPwa = async () => {
+  if (!deferredPrompt.value) {
+    toast.info('현재 브라우저에서는 앱 설치를 바로 진행할 수 없어요.', {
+      description: '설치 가능 환경이 아니거나 이미 설치된 상태일 수 있습니다.',
+    });
+    return;
+  }
+
+  deferredPrompt.value.prompt();
+
+  const choiceResult = await deferredPrompt.value.userChoice;
+
+  if (choiceResult.outcome === 'accepted') {
+    toast.success('앱 설치를 진행합니다.');
+  } else {
+    toast('앱 설치가 취소되었어요.');
+  }
+
+  deferredPrompt.value = null;
+  canInstall.value = false;
+};
+const showInstallToast = () => {
+  const toastId = toast.custom(() =>
+    h(
+      'div',
+      {
+        class:
+          'flex flex-col mx-auto items-center justify-center w-[150px] sm:w-[345px] bg-white/80 rounded-2xl p-5 shadow-lg border border-gray-100',
+      },
+      [
+        h(
+          'div',
+          { class: 'text-xs sm:text-sm font-medium text-slate-900 mb-2' },
+          '앱으로 더 편하게 이용해보세요'
+        ),
+        h(
+          'p',
+          { class: 'text-xs sm:text-sm text-slate-800 leading-relaxed mb-4' },
+          '홈 화면에 추가하면 캐비어를 빠르게 이용할 수 있어요 !'
+        ),
+        h('div', { class: 'flex justify-end gap-2' }, [
+          h(
+            'button',
+            {
+              class:
+                'px-4 py-1 rounded-lg bg-gray-100 text-slate-700  text-xs sm:text-sm hover:bg-gray-200',
+              onClick: () => toast.dismiss(toastId),
+            },
+            '취소'
+          ),
+          h(
+            'button',
+            {
+              class:
+                'px-4 py-1 rounded-lg bg-amber-400 text-white text-xs xm:text-sm font-semibold hover:bg-amber-500',
+              onClick: () => {
+                toast.dismiss(toastId);
+                installPwa();
+              },
+            },
+            '다운로드'
+          ),
+        ]),
+      ]
+    )
+  );
+};
+
+onMounted(() => {
+  showInstallToast();
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+});
 
 const date = computed({
   get: () => durationStore.date,
